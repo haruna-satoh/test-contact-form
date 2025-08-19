a8802526c44f6a4dc214660874a741d2b035c2dd
haruna <news.sigeak@gmail.com>
2025-08-19 21:30:57 +0900

    確認ページのレイアウト

--- src/resources/views/confirm.blade.php
+++ src/resources/views/confirm.blade.php
@@ -1,4 +1,58 @@
-<form action="/thanks" method="post">
-    @csrf
-    <button type="submit">送信</button>
-</form>
+<div class="confirm__contact">
+    <h2>Confirm</h2>
+    <form action="/thanks" method="post">
+        @csrf
+        <table class="confirm-table__inner">
+            <tr>
+                <th class="confirm-table__title">お名前</th>
+                <td class="confirm-table__item">
+                    <input type="text" name="name" value="{{ $name }}" readonly >
+                </td>
+            </tr>
+            <tr>
+                <th class="confirm-table__title">性別</th>
+                <td class="confirm-table__item">
+                    <input type="text" name="gender" value="{{ $gender }}" readonly >
+                </td>
+            </tr>
+            <tr>
+                <th class="confirm-table__title">メールアドレス</th>
+                <td class="confirm-table__item">
+                    <input type="text" name="email" value="{{ $email }}" readonly >
+                </td>
+            </tr>
+            <tr>
+                <th class="confirm-table__title">電話番号</th>
+                <td class="confirm-table__item">
+                    <input type="text" name="tel" value="{{ $tel }}" readonly>
+                </td>
+            </tr>
+            <tr>
+                <th class="confirm-table__title">住所</th>
+                <td class="confirm-table__item">
+                    <input type="text" name="address" value="{{ $address }}" readonly>
+                </td>
+            </tr>
+            <tr>
+                <th class="confirm-table__title">建物名</th>
+                <td class="confirm-table__item">
+                    <input type="text" name="building" value="{{ $building }}" readonly>
+                </td>
+            </tr>
+            <tr>
+                <th class="confirm-table__title">お問い合わせの種類</th>
+                <td class="confirm-table__item">
+                    <input type="text" name="select" value="{{ $select }}" readonly>
+                </td>
+            </tr>
+            <tr>
+                <th class="confirm-table__title">お問い合わせ内容</th>
+                <td class="confirm-table__item">
+                    <textarea name="contact" cols="30" rows="10" readonly>{{ $contact }}</textarea>
+                </td>
+            </tr>
+        </table>
+        <button type="submit">送信</button>
+        <a href="/">修正する</a>
+    </form>
+</div>
