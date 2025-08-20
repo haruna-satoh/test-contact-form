fda5338eb2fe5b274243266e7e58bc7d07d49b7b
haruna <news.sigeak@gmail.com>
2025-08-20 11:38:55 +0900

    Contactテーブルにダミーデータを挿入

---
+++ src/app/Models/Contact.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['category_id','first_name', 'last_name', 'gender', 'email', 'tel', 'address', 'building', 'detail'];
+
+    public static $rules = array(
+        'category_id' => 'required',
+        'first_name' => 'required',
+        'last_name' => 'required',
+        'gender' => 'required',
+        'email' => 'required',
+        'tel' => 'required',
+        'tel2' => 'required',
+        'tel3' => 'required',
+        'address' => 'required',
+        'detail' => 'required'
+    );
+}
