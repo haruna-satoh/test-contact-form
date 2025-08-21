4e71021b1c51d621caaf4dbf33940c15c0f41850
haruna <news.sigeak@gmail.com>
2025-08-21 16:16:53 +0900

    バリデーションを設定

---
+++ src/app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string',
+            'email' => 'required|string|email',
+            'password' => 'required|string'
+        ];
+    }
+
+    public function messages ()
+    {
+        return [
+            'name.required' => 'お名前を入力してください',
+            'name.string' => 'お名前を文字列で入力してください',
+            'email.required' => 'メールアドレスを入力してください',
+            'email.string' => 'メールアドレスを文字列で入力してください',
+            'email.email' => 'メールアドレスは「ユーザー名＠ドメイン」形式で入力してください',
+            'password.required' => 'パスワードを入力してください',
+            'password.string' => 'パスワードを文字列で入力してください'
+        ];
+    }
+}
