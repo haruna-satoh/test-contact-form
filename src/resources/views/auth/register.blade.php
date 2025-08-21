d9a38a8babadcac29d48a8364af0d0986ea567ef
haruna <news.sigeak@gmail.com>
2025-08-21 11:19:32 +0900

    新規登録ページのレイアウト

---
+++ src/resources/views/auth/register.blade.php
@@ -0,0 +1,41 @@
+<div class="register__contact">
+    <div class="register__title">
+        <h2>Register</h2>
+    </div>
+    <form action="/register" method="post">
+        @csrf
+        <div class="form__group">
+            <div class="form__group-title">
+                <span>お名前</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__group-item">
+                    <input type="text" name="name" placeholder="例: 山田 太郎" value="{{ old('name') }}">
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span>メールアドレス</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__group-item">
+                    <input type="text" name="email" placeholder="例: test@example.com" value="{{ old('email') }}">
+                </div>
+            </div>
+        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span>パスワード</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__group-item">
+                    <input type="text" name="password" placeholder="例: coachtech1106">
+                </div>
+            </div>
+        </div>
+        <div class="form__button">
+            <button type="submit">登録</button>
+        </div>
+    </form>
+</div>
