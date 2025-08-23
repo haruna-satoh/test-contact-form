9a28eba937ac386654c5b742f724665ce63aac51
haruna <news.sigeak@gmail.com>
2025-08-24 01:51:17 +0900

    クラス分け

--- src/resources/views/thanks.blade.php
+++ src/resources/views/thanks.blade.php
@@ -1,4 +1,22 @@
-<h2>お問い合わせありがとうございました</h2>
-<form action="/" method="get">
-    <button>HOME</button>
-</form>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/thanks.css') }}">
+    <title>ContactForm</title>
+</head>
+
+<body>
+    <div class="thanks__contact">
+        <div class="thanks__contact--title">
+            <h2>お問い合わせありがとうございました</h2>
+        </div>
+        <form action="/" method="get" class="form">
+            <button class="back-home" type="submit">HOME</button>
+        </form>
+    </div>
+</body>
+</html>
