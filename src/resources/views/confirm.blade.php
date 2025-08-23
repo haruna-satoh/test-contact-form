34d88fd880813294715b045eb81e53542231eccd
haruna <news.sigeak@gmail.com>
2025-08-24 00:51:54 +0900

    クラス分け

--- src/resources/views/confirm.blade.php
+++ src/resources/views/confirm.blade.php
@@ -5,66 +5,70 @@
 @endphp
 
 @section('css')
-    
+    <link rel="stylesheet" href="{{ asset('css/confirm.css') }}">
 @endsection
 
 @section('content')
     <div class="confirm__contact">
-        <h2>Confirm</h2>
-        <form action="/thanks" method="post">
+        <div class="confirm__title">
+            <h2>Confirm</h2>
+        </div>
+        <form class="form" action="/thanks" method="post">
             @csrf
-            <table class="confirm-table__inner">
+            <table class="confirm__table--inner">
                 <tr>
-                    <th class="confirm-table__title">お名前</th>
-                    <td class="confirm-table__item">
+                    <th class="confirm__table--title">お名前</th>
+                    <td class="confirm__table--item">
                         <input type="text" name="name" value="{{ $name }}" readonly >
                     </td>
                 </tr>
                 <tr>
-                    <th class="confirm-table__title">性別</th>
-                    <td class="confirm-table__item">
+                    <th class="confirm__table--title">性別</th>
+                    <td class="confirm__table--item">
                         <input type="text" name="gender" value="{{ $gender }}" readonly >
                     </td>
                 </tr>
                 <tr>
-                    <th class="confirm-table__title">メールアドレス</th>
-                    <td class="confirm-table__item">
+                    <th class="confirm__table--title">メールアドレス</th>
+                    <td class="confirm__table--item">
                         <input type="text" name="email" value="{{ $email }}" readonly >
                     </td>
                 </tr>
                 <tr>
-                    <th class="confirm-table__title">電話番号</th>
-                    <td class="confirm-table__item">
+                    <th class="confirm__table--title">電話番号</th>
+                    <td class="confirm__table--item">
                         <input type="text" name="tel" value="{{ $tel }}" readonly>
                     </td>
                 </tr>
                 <tr>
-                    <th class="confirm-table__title">住所</th>
-                    <td class="confirm-table__item">
+                    <th class="confirm__table--title">住所</th>
+                    <td class="confirm__table--item">
                         <input type="text" name="address" value="{{ $address }}" readonly>
                     </td>
                 </tr>
                 <tr>
-                    <th class="confirm-table__title">建物名</th>
-                    <td class="confirm-table__item">
+                    <th class="confirm__table--title">建物名</th>
+                    <td class="confirm__table--item">
                         <input type="text" name="building" value="{{ $building }}" readonly>
                     </td>
                 </tr>
                 <tr>
-                    <th class="confirm-table__title">お問い合わせの種類</th>
-                    <td class="confirm-table__item">
+                    <th class="confirm__table--title">お問い合わせの種類</th>
+                    <td class="confirm__table--item">
                         <input type="text" name="category_id" value="{{ $category }}" readonly>
                     </td>
                 </tr>
                 <tr>
-                    <th class="confirm-table__title">お問い合わせ内容</th>
-                    <td class="confirm-table__item">
+                    <th class="confirm__table--title">お問い合わせ内容</th>
+                    <td class="confirm__table--item">
                         <textarea name="content" cols="30" rows="10" readonly>{{ $content }}</textarea>
                     </td>
                 </tr>
             </table>
-            <button type="submit">送信</button>
-            <a href="/">修正する</a>
+            <div class="confirm__button">
+                <button class="confirm__button--submit" type="submit">送信</button>
+                <a class="form-back" href="/">修正する</a>
+            </div>
         </form>
     </div>
 @endsection
