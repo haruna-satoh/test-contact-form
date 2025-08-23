e2fd79013919e3f6622c2585f1df90b4a8e51bd6
haruna <news.sigeak@gmail.com>
2025-08-23 09:28:57 +0900

    クラス分け

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -5,45 +5,47 @@
 @endphp
 
 @section('css')
-    
+    <link rel="stylesheet" href="{{  asset('css/login.css') }}">
 @endsection
 
 @section('content')
     <div class="login__contact">
-        <div class="login-title">
+        <div class="login__title">
             <h2>Login</h2>
         </div>
-        <form action="/login" method="post" novalidate>
-            @csrf
-            <div class="form__group">
-                <div class="form__group-title">
-                    <span>メールアドレス</span>
+        <div class="login__card">
+            <form action="/login" method="post" novalidate>
+                @csrf
+                <div class="form__group">
+                    <div class="form__group--title">
+                        <span>メールアドレス</span>
+                    </div>
+                    <div class="form__group--item">
+                        <input type="email" name="email" placeholder="例: test@example.com" value="{{ old('email') }}">
+                        <div class="form__error">
+                            @error('email')
+                                {{ $message }}
+                            @enderror
+                        </div>
+                    </div>
                 </div>
-                <div class="form__group-item">
-                    <input type="email" name="email" placeholder="例: test@example.com" value="{{ old('email') }}">
+                <div class="form__group">
+                    <div class="form__group--title">
+                        <span>パスワード</span>
+                    </div>
+                    <div class="form__group--item">
+                        <input type="password" name="password" placeholder="coachtech1106">
+                        <div class="form__error">
+                            @error('password')
+                                {{ $message }}
+                            @enderror
+                        </div>
+                    </div>
                 </div>
-                <div class="form__error">
-                    @error('email')
-                        {{ $message }}
-                    @enderror
+                <div class="form__button">
+                    <button type="submit" class="form__button--submit">ログイン</button>
                 </div>
-            </div>
-            <div class="form__group">
-                <div class="form__group-title">
-                    <span>パスワード</span>
-                </div>
-                <div    class="form__group-item">
-                    <input type="password" name="password" placeholder="coachtech1106">
-                </div>
-                <div class="form__error">
-                    @error('password')
-                        {{ $message }}
-                    @enderror
-                </div>
-            </div>
-            <div class="form__button">
-                <button type="submit">ログイン</button>
-            </div>
-        </form>
+            </form>
+        </div>
     </div>
 @endsection
